78c7fde5c6c668baf815ce819b01d399b7aec661
Benson Wong <mostlygeek@gmail.com>
2013-04-19 12:12:52 -0700

    move aws command into its own file

--- enable-ec2.sh
+++ enable-ec2.sh
@@ -15,43 +15,4 @@ then
     export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
 fi
 
-function print_ec2Env {
-    echo ""
-    echo "EC2 Environment"
-    echo "---------------"
-    echo "EC2_PRIVATE_KEY  = $EC2_PRIVATE_KEY"
-    echo "EC2_CERT         = $EC2_CERT"
-    echo 
-}
-#
-# This is exported into the shell we can use setEC2Env as a CLI 
-# command
-function setEC2Env {
-    envPath="$credPath/$1"
-    if [ -e $envPath ]
-    then
-        pkFile="$envPath/pk.pem"
-        certFile="$envPath/cert.pem"
-
-        if [ ! -e $pkFile ]
-        then 
-            echo "ERROR: $pkFile missing"
-            return
-        fi
-
-        if [ ! -e $certFile ]
-        then 
-            echo "ERROR: $certFile missing"
-            return
-        fi
-        
-        export EC2_PRIVATE_KEY="$envPath/pk.pem"
-        export EC2_CERT="$envPath/cert.pem"
-        print_ec2Env
-    else
-        echo "ERROR: Unknown Environment: $1 ($envPath)"
-    fi
-}
-
-export -f setEC2Env
-
+alias aws="$basePath/aws.sh"
